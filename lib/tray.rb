13fef101b1a060ad0d9329575949165980450944
Francesco Canessa <makevoid@gmail.com>
2014-01-15 00:06:01 +0100

    tray

--- lib/tray.rb
+++ lib/tray.rb
@@ -1,6 +1,7 @@
 class Tray
   
   import java.awt.AWTException
+  import java.awt.TrayIcon
   import java.awt.SystemTray
   import java.awt.PopupMenu
   import java.awt.MenuItem
@@ -8,11 +9,14 @@ class Tray
   import java.awt.event.ActionEvent  
   import java.awt.event.ActionListener
   
+  PATH = File.expand_path "../../", __FILE__
+  
   def initialize
     image = Toolkit.getDefaultToolkit.getImage "#{PATH}/icon.png"
+    tray_icon = TrayIcon.new image, "DonaCoin"
     
-    tray = java.awt.SystemTray.getSystemTray  
-    puts tray
+    tray = java.awt.SystemTray.getSystemTray 
+    tray.add tray_icon 
   end
   
 end
